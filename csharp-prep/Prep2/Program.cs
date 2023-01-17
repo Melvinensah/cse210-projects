f3e99bb03cc1711bd20afe69ec41b0baab8c8aa5
Melvinensah <melvinbunduka@gmail.com>
2023-01-17 03:00:06 +0000

    Completed with pre 2 core requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("enter your grade percentage. ");
+        string userInput = Console.ReadLine();
+        int score = int.Parse(userInput);
+        
+        string letter = "";
+        
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        
+        else if (score >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (score >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (score  >= 60)
+        {
+            letter = "D";
+        }
+        
+        else
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine($"{score}--{letter}");
+        
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratulations you passed the test");
+        }
+        
+        else
+        {
+            Console.WriteLine("much more effort required");
+        }
     }
 }
