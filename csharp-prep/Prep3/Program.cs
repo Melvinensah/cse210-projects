a6e8359faf394e83a4c11a7af2ddcf9c15c1e16a
Melvinensah <melvinbunduka@gmail.com>
2023-01-25 07:56:15 +0000

    prep 3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+        int guess = -1;
+
+        
+        
+        while(guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("go higher");
+                
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("go lower");
+            }
+       
+            else
+            {
+                    Console.WriteLine("conagratulations your guess is right");
+            }
+        }
     }
 }
